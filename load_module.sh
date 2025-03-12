b73be2f33ea72e3e2fe9b03d2a2a1c59ae455a58
Florian Depraz <florian.depraz@outlook.com>
2025-03-12 14:59:22 +0000

    Exit on error
    Signed-off-by: Florian Depraz <florian.depraz@outlook.com>

--- load_module.sh
+++ load_module.sh
@@ -1,13 +1,11 @@
-#!/bin/sh -e
+#!/bin/sh
 # Load module script. Will remove the old one if it exists
 
 MODULE_NAME=ioctl
 MODULE_PATH="./dev/$MODULE_NAME.ko"
 
 # Remove module if it exists
-NEED_REMOVE=$(lsmod | grep "$MODULE_NAME")
-
-if [ ! -z "$NEED_REMOVE" ]; then
+if [ ! -z "$(lsmod | grep ${MODULE_NAME})" ]; then
     /sbin/rmmod ${MODULE_NAME} &&
         rm -f /dev/${MODULE_NAME} &&
         echo "Module removed"
@@ -17,11 +15,13 @@ if [ ! -z $1 ]; then
     MODULE_PATH=$1
 fi
 
+
+set -e
 MODULE_INFO="$(modinfo ${MODULE_PATH})"
 MODULE_ABSOLUTE_PATH="$(echo ${MODULE_INFO} | awk '/filename/{print $2}')"
 
-echo "Loading module ${MODULE_NAME} at ${MODULE_PATH} at ${MODULE_ABSOLUTE_PATH}"
 # Module can load only against the kernel it was compiled for
+echo "Loading module ${MODULE_NAME} at ${MODULE_PATH} at ${MODULE_ABSOLUTE_PATH}"
 
 # invoke insmod and specify the path to module .ko file.
 /sbin/insmod "${MODULE_PATH}" || exit 1
