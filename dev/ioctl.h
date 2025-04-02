c5b8b98c5bf636b539b1443e498b973ef4ae2eb1
Florian Depraz <florian.depraz@arm.com>
2025-04-02 18:04:55 +0200

    Add comment on ioctl numbers

--- dev/ioctl.h
+++ dev/ioctl.h
@@ -1,6 +1,9 @@
 #ifndef __IOCTL_DEFINE_H__
 #define __IOCTL_DEFINE_H__
 
+// See https://github.com/torvalds/linux/blob/v6.14/Documentation/userspace-api/ioctl/ioctl-number.rst
+//   for the list of ioctl number used as base.
+// Helps to avoid conflicts by having different commands if we talk to the wrong device.
 #define IOCTL_BASE 'W'
 #define IOCTL_GET_VAL _IO(IOCTL_BASE, 1)
 
